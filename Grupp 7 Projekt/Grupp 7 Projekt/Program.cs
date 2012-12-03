42f3c1500e74a6be28a1939e2ebee79ca3e2e3e9
Isiloron <isiloron@gmail.com>
2012-12-03 20:53:28 +0100

    Skapande

---
+++ Grupp 7 Projekt/Grupp 7 Projekt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Grupp_7_Projekt
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
