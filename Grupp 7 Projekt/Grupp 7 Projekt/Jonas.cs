3c8e6fcdffcc8a420092197178e2939a60f18d3c
Jonas T-H <jonaset@hotmail.com>
2012-12-05 11:54:50 +0100

    hejhej

---
+++ Grupp 7 Projekt/Grupp 7 Projekt/Jonas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Grupp_7_Projekt
+{
+    public partial class Jonas : Form
+    {
+        public Jonas()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtPassword1.Text == txtPassword2.Text)
+            {
+                label5.Text = "match";
+            }
+            else
+            {
+                label5.Text = "no match";
+            }
+        }
+    }
+}
