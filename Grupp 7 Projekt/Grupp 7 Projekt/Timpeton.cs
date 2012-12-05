e92c2370a87e2718517d2881d5b8d3e3f4a2c55a
Tim Thornell <tim.thornell91@gmail.com>
2012-12-05 12:13:02 +0100

    Tims arbete

---
+++ Grupp 7 Projekt/Grupp 7 Projekt/Timpeton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Grupp_7_Projekt
+{
+    public partial class Timpeton : Form
+    {
+        public Timpeton()
+        {
+            InitializeComponent();
+        }
+
+        private void Timpeton_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
