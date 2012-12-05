cb046fadbf7d814426587884a2579182cee869b2
Jonas T-H <jonaset@hotmail.com>
2012-12-05 11:54:50 +0100

    hejhej

---
+++ Grupp 7 Projekt/Grupp 7 Projekt/Jonas.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Grupp_7_Projekt
+{
+    partial class Jonas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(349, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Välkommen!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(299, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Du har inte bestämt ett lösenord än, vänligen gör det nedanför";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(350, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lösenord:";
+            // 
+            // txtPassword1
+            // 
+            this.txtPassword1.Location = new System.Drawing.Point(436, 167);
+            this.txtPassword1.Name = "txtPassword1";
+            this.txtPassword1.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword1.TabIndex = 3;
+            this.txtPassword1.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(293, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Verifiera lösenord:";
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.Location = new System.Drawing.Point(436, 206);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword2.TabIndex = 5;
+            this.txtPassword2.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Spara lösenord och logga in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(433, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 7;
+            // 
+            // Jonas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 694);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPassword2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Jonas";
+            this.Text = "Jonas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
--- Grupp 7 Projekt/Grupp 7 Projekt/Program.cs
+++ Grupp 7 Projekt/Grupp 7 Projekt/Program.cs
@@ -16,6 +16,7 @@ namespace Grupp_7_Projekt
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
+            Application.Run(new Jonas());
             //nilz waz alz0 here 2o12 
 			//detta är nästan magiskt /Linus
             //hejgay
