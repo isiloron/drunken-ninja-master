49e444d7d1381675f16178bde3b9a69637ff774e
Emblis <emblis.gr@gmail.com>
2012-12-14 11:34:08 +0100

    Recept + Ingr klasser

---
+++ ingridiensStruct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recept
+{
+    public struct Ingridient
+    {
+        public int Cal;
+        public int energy;
+        public string Name;
+        public string unit;
+        public string Description;
+    }
+}
