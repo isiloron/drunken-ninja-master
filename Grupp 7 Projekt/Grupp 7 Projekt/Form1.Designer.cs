fce1c857af4f346162dbc8ab99ccd0ff81c3c93b
Jonas T-H <jonaset@hotmail.com>
2012-12-20 17:41:50 +0100

    Inlogg och lösenhantering

--- Grupp 7 Projekt/Grupp 7 Projekt/Form1.Designer.cs
+++ Grupp 7 Projekt/Grupp 7 Projekt/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 55);
+            this.label1.Location = new System.Drawing.Point(91, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 0;
