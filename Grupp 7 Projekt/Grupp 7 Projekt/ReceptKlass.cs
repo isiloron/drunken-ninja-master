a462d2d1a1a3917665de05e8a612440f7e58cc35
Emblis <emblis.gr@gmail.com>
2012-12-28 17:00:53 +0100

    Flyttade in klasserna i grupp mappen

---
+++ Grupp 7 Projekt/Grupp 7 Projekt/ReceptKlass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recept
+{
+    public class Recept
+    {
+        string Titel; //Name of the recepie
+        string instructions; //Instructions on how to make it
+        List <string> IngrList = new List<string>(); //List of ingr to use
+        List<string> Tags = new List<string>(); //Tags related to this recpie
+        
+        public Recept(string Name, string instructions)
+        {
+            this.Titel = Name;
+            this.instructions = instructions;
+        }
+        
+        public void SetName(string NewName)
+        {//Used to Set a new name to recepie
+            Titel = NewName;        
+        }
+        
+        public void SetInstructions(string NewInstructions)
+        {//used to set new instructions
+            instructions = NewInstructions;
+        }
+        
+        public string GetName()
+        {//Returns the recepies name
+            return Titel;
+        }
+        
+        public string GetInstructions()
+        {//Retunrs instructions
+            return instructions;
+        }
+        
+        public void AddIngr (string IngrName)
+        {//Add new ingr to recepie
+            //Must somehow check if ingr exists
+            if (!true)
+            {
+                //Add ingr if it dont exist
+            }
+          IngrList.Add(IngrName);
+        }
+        
+        public string ReturnIngrNameByID(int ListID)
+        {//This one needs a better solution....
+            return IngrList[ListID]; 
+        }
+        
+        public bool RemoveIngrByName(string RemoveName) //Removes ingridiense by name, returns true of exisists, false if not
+        {
+            IngrList.Remove(RemoveName) ;
+            return true;
+        }
+
+        public int GetTotalCal() //Returns total calories for whole recepie
+        {
+            int Totalcal = 0;
+            for (int x = 0; x < IngrList.Count; x++)
+            {
+                //How to do this?
+            }
+          return Totalcal;
+        }
+        public int GetTotalEnergyValue() //Returns energy value for whole recepie
+        { 
+            int TotalEngergy =0;
+            for (int x = 0; x < IngrList.Count; x++)
+            { 
+                //how to do this?
+            }
+            return TotalEngergy;
+        }
+
+    }
+
+}
---
+++ Grupp 7 Projekt/Grupp 7 Projekt/ingridiensKlass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recept
+{
+    public class Ingridient
+    {
+        public int Cal;
+        public int energy;
+        public string Name;
+        public string unit;
+        public string Description;
+
+        public Ingridient(string NewName, string NewMessureUnit)
+        {
+            Name = NewName;
+            Unit = NewMessureUnit;
+        }
+        public Ingridient(string NewName, string NewMessureUnit, string NewDescription, int NewCal, int NewEnergy)
+        {
+            Name = NewName;
+            Unit = NewMessureUnit;
+            Description = NewDescription;
+            Cal = NewCal;
+            energy = NewEnergy;
+        
+        }
+    }
+}
