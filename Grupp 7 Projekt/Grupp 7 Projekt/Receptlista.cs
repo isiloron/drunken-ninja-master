9733de78da38ce2ee090965202d949f44ff0c4a1
fisbert <dsa12001@student.mdh.se>
2013-01-02 19:23:28 +0100

    Fler mockup metoder

--- Grupp 7 Projekt/Grupp 7 Projekt/Receptlista.cs
+++ Grupp 7 Projekt/Grupp 7 Projekt/Receptlista.cs
@@ -9,20 +9,30 @@ namespace Grupp_7_Projekt
     {
         List<Recept> receptlista;
         /*String.Join(Environment.NewLine, recept);*/
-        //En metod som hämtar receptlista från XMLserialiseraren(Linus kommer skriva en metod som gör att jag kan hämta alla recept)
-        //En metod som returnerar en stringlista med alla recepttitlar från receptlistan (Tim använder sig av den här)
+       
 
-        void hämta_receptlista()
+        public void hämta_receptlista()//En metod som hämtar receptlista från XMLserialiseraren(Linus kommer skriva en metod som gör att jag kan hämta alla recept
+        {
+            foreach (Recept r in receptlista);
+         
+        }
+
+        public visa_titlar()//En metod som returnerar en stringlista med alla recepttitlar från receptlistan (Tim använder sig av den här)
+        {
+            String.Join(Environment.NewLine, Recept);
+        }
+
+        public void Lagg_till()
         {
 
         }
 
-        void visa_titlar()
+        public void Ta_bort()
         {
- 
+
         }
 
-        void sök_recept()
+        public void sök_recept() //En metod som gör att man ska kunna söka i receptlistan
         {
  
         }
