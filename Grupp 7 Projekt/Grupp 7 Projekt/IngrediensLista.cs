dad55368137f81d9d89ed863f91e41d8c0cfe2d1
Nils Krawczuk <nkk12001@gmail.com>
2013-01-03 18:08:06 +0100

    IngrediensLista

--- Grupp 7 Projekt/Grupp 7 Projekt/IngrediensLista.cs
+++ Grupp 7 Projekt/Grupp 7 Projekt/IngrediensLista.cs
@@ -7,6 +7,48 @@ namespace Grupp_7_Projekt
 {
     class IngrediensLista
     {
-       //metoder som hämtar, visas, lägg till, ändras, söker
+       List<Ingredient> ingredienslista;
+
+       public IngrediensLista()
+       {
+           NyIngrediensBok();
+       }
+        
+        public void LaddaIngrediens()
+        {
+            SparaOchLaddaIngredienser.LaddaXML(out ingredienslista);
+        }
+
+        public void NyIngrediensBok()
+        {
+            ingredienslista = new List<Ingredient>();
+        }
+
+        public List<string> HämtaIngTitlar()
+        {
+            List<string> TitelIngLista = new List<string>();
+            foreach (Ingredient i in ingredienslista)
+            {
+                TitelIngLista.Add(i.Name);
+            }
+            return TitelIngLista;
+                                  
+        }
+        public void LäggTillIng()
+        { 
+
+        }
+        public void TaBortIng(string name)
+        {
+            foreach (Ingredient i in ingredienslista)
+            {
+                if (i.Name.Equals(name))
+                    ingredienslista.Remove(i);
+            }
+        }
+        public void SökIng()
+        {
+
+        }
     }
 }
