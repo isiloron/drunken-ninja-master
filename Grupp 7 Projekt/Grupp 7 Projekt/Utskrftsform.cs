09b2183e2671325a4def07dfe97923ea51edf746
Nils Krawczuk <nkk12001@gmail.com>
2013-01-08 18:47:02 +0100

    Skriv ut Fungerar(buggfix)

--- Grupp 7 Projekt/Grupp 7 Projekt/Utskrftsform.cs
+++ Grupp 7 Projekt/Grupp 7 Projekt/Utskrftsform.cs
@@ -12,6 +12,8 @@ namespace Grupp_7_Projekt
 {
     public partial class Utskrftsform : Form
     {
+        private Button printButton = new Button();
+        private PrintDocument printDocument1 = new PrintDocument();
         public Utskrftsform(TextBox textBoxIngr, ComboBox comboBoxPort, Label lblTitel, TextBox textBoxnär, TextBox textBoxTil, PictureBox pictureBox1)
         {
 
@@ -23,6 +25,10 @@ namespace Grupp_7_Projekt
             textBoxtilut.Text = textBoxTil.Text;
             pictureBoxut.Image = pictureBox1.Image;
             pictureBoxbakgrund.SendToBack();
+
+            printButton.Click += new EventHandler(btnskrivut_Click);
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            this.Controls.Add(printButton);
         }
 
         private void Utskrftsform_Load(object sender, EventArgs e)
@@ -32,37 +38,27 @@ namespace Grupp_7_Projekt
 
         private void btnskrivut_Click(object sender, EventArgs e)
         {
-            
-            System.Drawing.Printing.PrintDocument Doc = new System.Drawing.Printing.PrintDocument();
-            Doc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.Doc_PrintPage123);
-            Doc.DefaultPageSettings.Landscape = true;
-            Doc.DefaultPageSettings.PrinterSettings.DefaultPageSettings.Landscape = true;
-            Doc.DefaultPageSettings.PrinterSettings.Copies = 1;
-            Doc.PrinterSettings.Copies = 1;
-            Doc.Print();
+            CaptureScreen();
+            printDocument1.Print();  
+          
         }
-
- private void Doc_PrintPage123(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        Bitmap memoryImage;
+        private void CaptureScreen()
         {
-            Bitmap bitmap = new Bitmap(this.Width, this.Height);
-            this.DrawToBitmap(bitmap, new Rectangle(0, 0, bitmap.Width, bitmap.Height));
-            e.Graphics.DrawImage(bitmap, 0, 0);
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = this.Size;
+            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
         }
 
-        
+        private void printDocument1_PrintPage(System.Object sender,
+               System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(memoryImage, 0, 0);
+        }
 
- 
 
-            //PrintDocument pd = new PrintDocument();
-            //PrintDialog wtf = new PrintDialog();
-            //wtf.Document = pd;
-            //PrintPreviewDialog Preview = new PrintPreviewDialog();
-            //Preview.Document = pd;
-            //Preview.ShowDialog();
-            //if (wtf.ShowDialog() == DialogResult.OK)
-            //{
-            //    pd.Print();
-            //}
         }
  
 }
