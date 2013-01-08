71fb9451fae4110efbcc9e1cb3691856c7e7bd4c
Nils Krawczuk <nkk12001@gmail.com>
2013-01-08 19:50:39 +0100

    kommenterat skriv ut metoderna

--- Grupp 7 Projekt/Grupp 7 Projekt/Utskrftsform.cs
+++ Grupp 7 Projekt/Grupp 7 Projekt/Utskrftsform.cs
@@ -12,8 +12,7 @@ namespace Grupp_7_Projekt
 {
     public partial class Utskrftsform : Form
     {
-        private Button printButton = new Button();
-        private PrintDocument printDocument1 = new PrintDocument();
+        private PrintDocument SkrivutDok = new PrintDocument();
         public Utskrftsform(TextBox textBoxIngr, ComboBox comboBoxPort, Label lblTitel, TextBox textBoxnär, TextBox textBoxTil, PictureBox pictureBox1)
         {
 
@@ -26,9 +25,9 @@ namespace Grupp_7_Projekt
             pictureBoxut.Image = pictureBox1.Image;
             pictureBoxbakgrund.SendToBack();
 
-            printButton.Click += new EventHandler(btnskrivut_Click);
-            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
-            this.Controls.Add(printButton);
+           
+            SkrivutDok.PrintPage += new PrintPageEventHandler(SkrivutDok_PrintSida);
+            
         }
 
         private void Utskrftsform_Load(object sender, EventArgs e)
@@ -36,26 +35,25 @@ namespace Grupp_7_Projekt
 
         }
 
-        private void btnskrivut_Click(object sender, EventArgs e)
+        private void btnskrivut_Click(object sender, EventArgs e) //När man trycker på Skriv ut knappen anopas metoderna
         {
-            CaptureScreen();
-            printDocument1.Print();  
+            SparaBild();
+            SkrivutDok.Print();  
           
         }
-        Bitmap memoryImage;
-        private void CaptureScreen()
+        Bitmap memoryBild;
+        private void SparaBild() //tar printscreen på fönstret och sparar bilden på minnet
         {
-            Graphics myGraphics = this.CreateGraphics();
+            Graphics Bild = this.CreateGraphics();
             Size s = this.Size;
-            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
-            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
+            memoryBild = new Bitmap(s.Width, s.Height, Bild);
+            Graphics memoryGrafik= Graphics.FromImage(memoryBild);
+            memoryGrafik.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
         }
 
-        private void printDocument1_PrintPage(System.Object sender,
-               System.Drawing.Printing.PrintPageEventArgs e)
+        private void SkrivutDok_PrintSida(System.Object sender, System.Drawing.Printing.PrintPageEventArgs e)//skriver ut
         {
-            e.Graphics.DrawImage(memoryImage, 0, 0);
+            e.Graphics.DrawImage(memoryBild, 0, 0);
         }
 
 
